58ffdd1a5a0958dac8094c2bf196652971d90d45
audrbar <audrbar@gmail.com>
2023-02-24 22:38:33 +0200

    onAdd

--- src/App.js
+++ src/App.js
@@ -1,49 +1,61 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Tasks from './components/Tasks';
+import { useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors appointment",
+      day: "2023-02-29",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at Scholl",
+      day: "2023-04-15",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "2023-03-07",
+      reminder: false,
+    },
+  ]);
 
-  const [tasks, setTasks] = useState ([
-    {
-        id: 1,
-        text: 'Doctors appointment',
-        day: '2023-02-29',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at Scholl',
-        day: '2023-04-15',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: '2023-03-07',
-        reminder: false,
-    },
-]);
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
-  }
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
-return (
+  return (
     <div className="container">
-        <Header title="Task Tracker" />
-        {tasks.length > 0 ? (
-        <Tasks tasks={tasks}
-        onDelete={deleteTask}
-        onToggle={toggleReminder}
-        />
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!setShowAddTask)}
+      />
+      {showAddTask && <AddTask onAdd={AddTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
-        'There is no tasks to show.'
+        "There is no tasks to show."
       )}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,21 @@
-import PropTypes from 'prop-types';
-import Button from './Button';
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => {
-
-    const onClick = () => {
-        console.log('Click');
-    }
-
+const Header = ({ title, onAdd }) => {
   return (
-    <header className='header'>
-        <h2>{title}</h2>
-        <Button color='green' text='Add' onClick={onClick} />
+    <header className="header">
+      <h2>{title}</h2>
+      <Button color="green" text="Add" onClick={onAdd} />
     </header>
-  )
-}
-
-
-Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  );
+};
 
 Header.defaultProps = {
-    title: PropTypes.string,
-}
+  title: "Task Tracker",
+};
+
+Header.defaultProps = {
+  title: PropTypes.string,
+};
 
 export default Header;
