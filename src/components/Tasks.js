f3118d95708f7581ded5742a3d408cc99ccce6ce
audrbar <audrbar@gmail.com>
2023-02-26 22:54:46 +0200

    fetch and routing

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,18 @@
-import PropTypes from 'prop-types';
-import Task from './Task';
+import PropTypes from "prop-types";
+import Task from "./Task";
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
-
   return (
     <>
-        {tasks.map((task) => (
-        <Task
-        key={task.id}
-        task={task}
-        onDelete={onDelete}
-        onToggle={onToggle}
-        />
-        ))}
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
     </>
-  )
-}
+  );
+};
 
 Tasks.defaultProps = {
-    text: PropTypes.string,
-}
+  text: PropTypes.string,
+};
 
 export default Tasks;
